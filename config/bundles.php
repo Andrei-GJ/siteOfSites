2075d920932387130f0593046766af068f44171d
Andrei-GJ <andreisotelo369@gmail.com>
2026-07-04 00:51:33 -0500

    Make MakerBundle registration conditional

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,14 @@
 <?php
 
-return [
+$bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
+
+if (class_exists(Symfony\Bundle\MakerBundle\MakerBundle::class)) {
+    $bundles[Symfony\Bundle\MakerBundle\MakerBundle::class] = ['dev' => true];
+}
+
+return $bundles;
